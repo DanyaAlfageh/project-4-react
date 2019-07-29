34cb10919866c3ba7d4ad3e763fff1dc87f9e028
Danya <dalfakih@yahoo.com>
2019-07-29 15:55:09 +0300

    deployment

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://protected-lowlands-72109.herokuapp.com/',
   development: 'http://localhost:3000'
 }
 
